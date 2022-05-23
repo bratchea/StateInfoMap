39480cb1473b52f99d4542c9eb02c3f7d0b3cbf7
Adam Bratcher <bratchea@oregonstate.edu>
2022-05-22 21:16:38 -0400

    fix typo

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("click", (e) => {
     if (e.target.tagName == "path") {
         let content = e.target.dataset.name;
-        window.location = `ttp://state-info-proj.uk.r.appspot.com/state/${content}`;
+        window.location = `http://state-info-proj.uk.r.appspot.com/state/${content}`;
     } else {
         console.log("Not Clickable");
     }
