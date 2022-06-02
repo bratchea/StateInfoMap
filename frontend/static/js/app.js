3727a214c235157225bbe70fb81aefd1848cb7fd
Adam Bratcher <bratchea@oregonstate.edu>
2022-06-02 10:42:58 -0400

    state abbreviation on hover

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -6,3 +6,19 @@ document.addEventListener("click", (e) => {
         console.log("Not Clickable");
     }
 });
+
+document.addEventListener("mouseover", (e) => {
+    if (e.target.tagName == "path") {
+        let box = document.getElementById("info-box");
+        let x = e.clientX + "px";
+        let y = e.clientY + "px";
+        let content = e.target.dataset.id;
+
+        box.style.display = "flex";
+        box.style.top = y;
+        box.style.left = x;
+        box.innerText = content;
+    } else {
+        document.getElementById("info-box").style.display = "none";
+    }
+});
